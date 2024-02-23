b898ffc00b45b9118dd58a3ca5113077a99e370e
Ro-woon <125912215+jenkinsyoung@users.noreply.github.com>
2024-02-23 17:32:40 +0000

    modified:   storeat/components/showFullRecipe.js 	modified:   storeat/pages/my_recipes.js 	modified:   storeat/styles/fullRecipe.module.css

--- storeat/components/showFullRecipe.js
+++ storeat/components/showFullRecipe.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import style from '../styles/fullRecipe.module.css'
-
+import Image from 'next/image';
 
 export class ShowFullRecipe extends Component {
   render() {
     return (
       <div className={style.fullRecipe} onClick={() => this.props.onShowRecipe(this.props.recipe)}>
+            <div style={{display: 'flex', justifyContent:'center'}}>
             <h2 className={style.title}>{this.props.recipe.title}</h2>
+            <div style={{top: '50%', marginTop: '25px', marginLeft: '5px'}}>
+            <Image
+                src="/pencil.svg"
+                width={22}
+                height={22}
+                alt=""
+            />
+            </div>
+            </div>
+           
             <div className={style.wrapper}>
             <div className={style.img}><img src={"/"+ this.props.recipe.img} /></div>
             <div className={style.ingredients}>
@@ -25,7 +36,12 @@ export class ShowFullRecipe extends Component {
             </div>
             </div>
             <p className={style.mini_title}>Описание</p>
-            <p className={style.desc}>{this.props.recipe.desc}</p>
+            <div className={style.container}>
+            {this.props.recipe.desc.map(el=>(
+                <p className={style.desc} key={el.id}>{el.id}. {el.description}</p>
+            ))}
+            </div>
+            
       </div>
     )
   }

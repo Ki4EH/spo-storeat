7863962fcccd7e2fd8b66d384a68c48db0667668
sv022 <ssvatkin04@gmail.com>
2024-03-02 17:16:04 +0300

    user/expiredproducts request

--- app/routes/nodeRoutes.js
+++ app/routes/nodeRoutes.js
@@ -1,14 +1,30 @@
 module.exports = function(app, db) {
-    app.post('/product', (req, res) => {
-        const entry = {userID: req.body.userID, name: req.body.name, date: req.body.date};
+    app.post('/user/newproduct', (req, res) => {
+        const entry = req.body.product;
+        const userId = req.body.id;
         // console.log(req.body);
-        db.collection('products').insertOne(entry).then((result, err) => {
+        db.collection('users').updateOne({id: userId}, {$push: {products: entry}}).then((result, err) => {
             if (err){
                 res.send({"error": "An error has occured."});
                 console.log(err)
             } 
-            else res.send("Done.");
-        })
+            else res.send("Done."); 
+        }
+        )
+    });
+
+    app.post('/user/newrecipe', (req, res) => {
+        const entry = req.body.recipe;
+        const userId = req.body.id;
+        // console.log(req.body);
+        db.collection('users').updateOne({id: userId}, {$push: {recipes: entry}}).then((result, err) => {
+            if (err){
+                res.send({"error": "An error has occured."});
+                console.log(err)
+            } 
+            else res.send("Done."); 
+        }
+        )
     });
 
     app.get("/user/products", (req, res) => {
@@ -32,6 +48,22 @@ module.exports = function(app, db) {
                 res.send({"error": "server error has occured"});
             } 
             else res.send(item.recipes);
+        });
+    });
+
+    app.get("/user/expiredproducts", (req, res) => {
+        const userId = parseInt(req.body.userID);
+        db.collection("users").findOne({id: userId}, {}).then((item, err) => {
+            // console.log(userId);
+            if (err) {
+                console.log(err);
+                res.send({"error": "server error has occured"});
+            } 
+            item.products.forEach(element => {
+                let datestr = element.expiresBy;
+                
+            });
+            
         });
     });
 };

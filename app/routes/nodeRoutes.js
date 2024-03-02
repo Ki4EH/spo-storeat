b6bbfcffbd8fa10f86f7a01a8d955f22a51a5b68
sv022 <ssvatkin04@gmail.com>
2024-03-02 16:28:17 +0300

    added users/products and users/recipes

--- app/routes/nodeRoutes.js
+++ app/routes/nodeRoutes.js
@@ -12,27 +12,26 @@ module.exports = function(app, db) {
     });
 
     app.get("/user/products", (req, res) => {
-        const userId = req.body.userID;
-        console.log(userId);
-        console.log(`${userId}`);
-        // db.collection(`${userId}`).count().then((result, err) => {
-        //     if (err){
-        //         res.send({"error": "An error has occured."});
-        //         console.log(err)
-        //     } 
-       
-        //     else {
-        //         console.log(result);
-        //         res.send("Done");
-        //     } 
-        // });
-        db.collection(`${userId}`).findOne({type: "products"}, {_id: 0, products: 1, type: 0}).then((item, err) => {
-            console.log(userId);
+        const userId = parseInt(req.body.userID);
+        db.collection("users").findOne({id: userId}, {}).then((item, err) => {
+            // console.log(userId);
             if (err) {
                 console.log(err);
                 res.send({"error": "server error has occured"});
             } 
-            else res.send(item);
+            else res.send(item.products);
+        });
+    });
+
+    app.get("/user/recipes", (req, res) => {
+        const userId = parseInt(req.body.userID);
+        db.collection("users").findOne({id: userId}, {}).then((item, err) => {
+            // console.log(userId);
+            if (err) {
+                console.log(err);
+                res.send({"error": "server error has occured"});
+            } 
+            else res.send(item.recipes);
         });
     });
 };

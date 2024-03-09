172aee9ca7adcbfc7dccb2696eccd77ca4a58a65
sv022 <ssvatkin04@gmail.com>
2024-03-09 17:58:43 +0300

    started working on proper error handling

--- app/routes/nodeRoutes.js
+++ app/routes/nodeRoutes.js
@@ -1,3 +1,5 @@
+const { Error } = require("mongoose");
+
 function formatDate(date){
     return `${Math.floor(date / 10)}${date % 10}`
 }
@@ -23,8 +25,14 @@ module.exports = function(app, db) {
     });
 
     app.post('/user/newrecipe', (req, res) => { // добавление рецепта пользователя
-        const entry = req.body.recipe;
-        const userId = req.body.id;
+        let entry;
+        let userId;
+        try {
+            entry = req.body.recipe;
+            userId = req.body.id;
+        } catch {
+            res.send({"Error": "Incorrect request."});
+        }
         // console.log(req.body);
         db.collection('users').updateOne({id: userId}, {$push: {recipes: entry}}).then((result, err) => {
             if (err){
@@ -37,28 +45,46 @@ module.exports = function(app, db) {
     });
 
     app.post('/user/openproduct', (req, res) => { // добавление рецепта пользователя
-        const productId = req.body.productId;
-        const userId = req.body.id;
+        let userId = 0;
+        let productId = 0;
+        try {
+            userId = parseInt(req.query.id);
+            productId = parseInt(req.query.productId);
+        } catch {
+            res.send({"Error": "Incorrect request."});
+        }
         let date = new Date();
         date = `${formatDate(date.getFullYear())}-${formatDate(date.getUTCMonth() + 1)}-${formatDate(date.getDate())}`;
         db.collection('users').updateOne({id: userId, "products.id": productId}, {$set: {"products.$.openingDate" : date}}).then((result, err) => {
             if (err){
                 res.send({"error": "An error has occured."});
                 console.log(err)
-            } 
+            }
+            else if (!result) {
+                res.send({"error": {"Error": "Could not find product with ID " + productId + " from user with ID " + userId + "."}});
+                console.log(err)
+            }
             else res.send("Done."); 
         }
         )
     });
 
     app.get("/user/products", (req, res) => { // получение продукта пользователя
-        const userId = parseInt(req.query.id);
+        let userId = 0;
+        try {
+            userId = parseInt(req.query.id);
+        } catch {
+            res.send({"Error": "Incorrect request."})
+        }
         db.collection("users").findOne({id: userId}, {}).then((item, err) => {
             // console.log(userId);
             if (err) {
                 console.log(err);
-                res.send({"error": "server error has occured"});
+                res.send({"Error": "Server error has occured."});
             } 
+            else if (!item) {
+                res.send({"Error": "Could not find user with ID " + userId + "."})
+            }
             else res.send(item.products);
         });
     });
@@ -77,18 +103,20 @@ module.exports = function(app, db) {
 
     app.get("/user/expiredproducts", (req, res) => { // получение просроченных продуктов пользователя
         const userId = parseInt(req.query.id);
+        console.log('1');
         db.collection("users").findOne({id: userId}, {}).then((item, err) => {
             // console.log(userId);
             if (err) {
                 console.log(err);
                 res.send({"error": "server error has occured"});
             }
+            
             let expiredproducts = [];
             let index = 1;
             item.products.forEach(element => {
                 if (element.openingDate){
                     let newDate = new Date(element.openingDate);
-                    console.log(element.openingDate, element.name, newDate);
+                    // console.log(element.openingDate, element.name, newDate);
                     if (newDate.getTime() + (element.shelfLife * 24 * 3600 * 1000) < new Date().getTime()) { 
                         expiredproducts.push({
                             id: index++,

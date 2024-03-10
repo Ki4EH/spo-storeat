e2ed42382539d2652d20befc352eab6d0c90c902
Ro-woon <125912215+jenkinsyoung@users.noreply.github.com>
2024-03-10 06:10:33 +0000

    deleted:    .gitignore 	deleted:    README copy.md 	deleted:    next.config.mjs 	deleted:    package-lock.json 	deleted:    package.json 	deleted:    postcss.config.js 	deleted:    public/Line.svg 	deleted:    public/Login.svg 	deleted:    public/add.svg 	deleted:    public/arrow.svg 	deleted:    public/background_1.svg 	deleted:    public/close.svg 	deleted:    public/cutlery.svg 	deleted:    public/dairy-products.svg 	deleted:    public/image-1.jpg 	deleted:    public/image-2.jpg 	deleted:    public/image-3.jpg 	deleted:    public/image-4.jpg 	deleted:    public/image-editing 4.svg 	deleted:    public/leaves-2-svgrepo-com 1.svg 	deleted:    public/main-page_image.svg 	deleted:    public/menu.svg 	deleted:    public/pencil.svg 	deleted:    public/plus.svg 	deleted:    public/remove.svg 	deleted:    src/Url.js 	deleted:    src/app/(lk)/LK_layout.module.scss 	deleted:    src/app/(lk)/layout.tsx 	deleted:    src/app/(lk)/menu/MenuPage.module.scss 	deleted:    src/app/(lk)/menu/page.tsx 	deleted:    src/app/(lk)/products/ProductPage.module.scss 	deleted:    src/app/(lk)/products/[id]/ProductPage.module.scss 	deleted:    src/app/(lk)/products/[id]/page.tsx 	deleted:    src/app/(lk)/products/page.tsx 	deleted:    src/app/(lk)/recipes/RecipePage.module.scss 	deleted:    src/app/(lk)/recipes/[id]/RecipePage.module.scss 	deleted:    src/app/(lk)/recipes/[id]/page.tsx 	deleted:    src/app/(lk)/recipes/page.tsx 	deleted:    src/app/HomePage.module.scss 	deleted:    src/app/globals.css 	deleted:    src/app/layout.tsx 	deleted:    src/app/page.tsx 	deleted:    src/components/Add.tsx 	deleted:    src/components/AddProduct.tsx 	deleted:    src/components/AddRecipe.tsx 	deleted:    src/components/Categories.tsx 	deleted:    src/components/DeleteProduct.tsx 	deleted:    src/components/DeleteRecipe.tsx 	deleted:    src/components/FilteredProduct.tsx 	deleted:    src/components/Links.tsx 	deleted:    src/components/Meal.tsx 	deleted:    src/components/Meals.tsx 	deleted:    src/components/NavLink.tsx 	deleted:    src/components/ProductForm.tsx 	deleted:    src/components/ProductSlider.tsx 	deleted:    src/components/Products.tsx 	deleted:    src/components/Profile.tsx 	deleted:    src/components/RecipeForm.tsx 	deleted:    src/components/Recipes.tsx 	deleted:    src/components/ShopList.tsx 	deleted:    src/components/Sidebar.tsx 	deleted:    src/components/Slider.tsx 	deleted:    src/components/SliderItem.tsx 	deleted:    src/components/Title.tsx 	deleted:    src/components/TitleDescription.tsx 	deleted:    src/interfaces/category.inteface.ts 	deleted:    src/interfaces/meal.interface.ts 	deleted:    src/interfaces/product.interface.ts 	deleted:    src/interfaces/productSlider.interface.ts 	deleted:    src/interfaces/recipe.interface.ts 	deleted:    src/interfaces/shoplist.interface.ts 	deleted:    src/styles/Categories.module.scss 	deleted:    src/styles/DeleteButton.module.scss 	deleted:    src/styles/Links.module.scss 	deleted:    src/styles/Meal.module.scss 	deleted:    src/styles/Meals.module.scss 	deleted:    src/styles/NavLink.module.scss 	deleted:    src/styles/ProductForm.module.scss 	deleted:    src/styles/Profile.module.scss 	deleted:    src/styles/RecipeForm.module.scss 	deleted:    src/styles/Recipes.module.scss 	deleted:    src/styles/Sidebar.module.scss 	deleted:    src/ui/MenuProductItem.module.scss 	deleted:    src/ui/MenuProductItem.tsx 	deleted:    src/ui/MenuRecipeItem.module.scss 	deleted:    src/ui/MenuRecipeItem.tsx 	deleted:    src/ui/ProductItem.module.scss 	deleted:    src/ui/ProductItem.tsx 	deleted:    src/ui/RecipeItem.module.scss 	deleted:    src/ui/RecipeItem.tsx 	deleted:    tailwind.config.ts 	deleted:    tsconfig.json

--- src/Url.js
+++
@@ -1,2 +0,0 @@
-const URL = 'http://192.168.137.221:8000/user'
-export default URL
--- src/interfaces/shoplist.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IShopList{
-    id: number
-    name: string
-    weight: number
-}
-
-export interface IShopListData{
-    elements: IShopList[]
-}
